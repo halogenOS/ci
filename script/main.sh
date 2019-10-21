ca92364a570f56987c144d395cc35b2908164fb4
Simao Gomes Viana <xdevs23@outlook.com>
2019-10-21 21:47:53 +0200

    prepare for cloud
    Change-Id: I5a5ac078398dd2eb16e639be206ee044f834bf17

--- script/main.sh
+++ script/main.sh
@@ -3,8 +3,11 @@
 function main() {
   echo "ROM Build Script"
   echo
+  export ROM_VERSION_ONLY="$(echo ${XOS_REVISION} | cut -d '-' -f2)"
+  pushd /mnt/xos/$ROM_VERSION_ONLY/building/ws
   run src_sync 
   run rom_build
   run rom_upload
+  popd
 }
 
